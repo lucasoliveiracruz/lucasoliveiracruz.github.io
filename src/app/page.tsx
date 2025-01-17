4ada74a684867ac4d28bea438ffa60ac9e0d2a05
Lucas Cruz <lucasoliveira.cruz@loggi.com>
2025-01-17 12:14:38 -0300

    feat: remove sessions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Home from '../components/Home'
-import About from '../components/About'
-import Career from '../components/Career'
-import Projects from '../components/Projects'
+// import About from '../components/About'
+// import Career from '../components/Career'
+// import Projects from '../components/Projects'
 import Contact from '../components/Contact'
 
 export default function Page() {
   return (
     <main className="min-h-screen bg-gray-100">
       <Home />
-      <About />
-      <Career />
-      <Projects />
+      {/* <About /> */}
+      {/* <Career /> */}
+      {/* <Projects /> */}
       <Contact />
     </main>
   )
